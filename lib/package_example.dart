32d5c8998bfd775c3cc59997dbae61d473d9c8bf
Antony Ruby Alexia <alexia@m2p.in>
2021-11-22 17:45:19 +0530

    added alertbox

--- lib/package_example.dart
+++ lib/package_example.dart
@@ -1,4 +1,36 @@
 library package_example;
 
-/// A Calculator.
-class Text {}
+import 'package:flutter/material.dart';
+
+class AlertBox {
+  static Future showCustomAlertBox({
+    required BuildContext context,
+    required Widget willDisplayWidget,
+  }) {
+    assert(context != null, "context is null!!");
+    assert(willDisplayWidget != null, "willDisplayWidget is null!!");
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                willDisplayWidget,
+                MaterialButton(
+                  color: Colors.white30,
+                  child: Text('close alert'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+            elevation: 10,
+          );
+        });
+  }
+}
